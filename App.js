83d95f1ff851740a55885734ace0cf2de2e48e56
farabi <farabi.djauhari@gmail.com>
2019-02-19 11:24:16 +1100

    First Commit

---
+++ App.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { StyleSheet, Platform, Image, Text, View, ImageBackground, SafeAreaView } from 'react-native';
+
+import firebase from 'react-native-firebase';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  async componentDidMount() {
+    // TODO: You: Do firebase things
+    // const { user } = await firebase.auth().signInAnonymously();
+    // console.warn('User -> ', user.toJSON());
+
+    // await firebase.analytics().logEvent('foo', { bar: '123'});
+  }
+
+  render() {
+    return (
+      <ImageBackground source={require('./assets/background.png')} style={styles.containerBackground} >
+        <SafeAreaView style={styles.container}>
+          <Image source={require('./assets/ReactNativeFirebase.png')} style={[styles.logo]}/>
+        </SafeAreaView>
+      </ImageBackground>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  containerBackground: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 25 : 0
+  },
+  logo: {
+    height: 120,
+    marginBottom: 16,
+    marginTop: 64,
+    padding: 10,
+    width: 135,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  modules: {
+    margin: 20,
+  },
+  modulesHeader: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  module: {
+    fontSize: 14,
+    marginTop: 4,
+    textAlign: 'center',
+  }
+});
