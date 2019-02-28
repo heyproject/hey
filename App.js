3c965f2b2cf23bd326d1ec5163c00394b4468a49
Hey <heyprojectmain@gmail.com>
2019-03-01 00:56:21 +1100

    clean up code

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import AppContainer from './MainNavigation';
 export default class App extends React.Component {
   render() {
-    const { started, error, codeInput, sent, auto, user } = this.state;
     return (
       <AppContainer />
     )
