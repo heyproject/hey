1de5032ab0af228d34c5a289a19b389ff538e71a
farabi <farabi.djauhari@gmail.com>
2019-03-05 20:43:14 +1100

    Android fix

--- android/app/src/main/java/com/heycompany/hey/MainApplication.java
+++ android/app/src/main/java/com/heycompany/hey/MainApplication.java
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import io.invertase.firebase.RNFirebasePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -46,8 +44,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.asList(
         new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RNFirebasePackage(),
             new RNGestureHandlerPackage(),
         new RNFirebasePackage(),
         // add/remove these packages as appropriate
